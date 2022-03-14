7547243a6b3d68453f0436adbfd12d3b8efd3a38
azmir849 <azmir15-849@diu.edu.bd>
2022-03-15 01:33:04 +0600

    update folder structure for production

--- e-tech-server/index.js
+++ e-tech-server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 8000;
-
+const path = require('path')
 
 app.use(express.json());
 app.use(cors());
@@ -85,9 +85,9 @@ client.connect(err => {
     
 });
 
-app.use(express.static(path.join(__dirname, './e-tech-agency/e-tech-client/build')));
+app.use(express.static(path.join(__dirname, './e-tech-client/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './e-tech-agency/e-tech-client/build/index.html'))
+  res.sendFile(path.join(__dirname, './e-tech-client/build/index.html'))
 );
 
 // app.get('/', (req, res) => {
